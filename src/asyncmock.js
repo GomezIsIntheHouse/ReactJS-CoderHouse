e0bb2952d2876c1c1152b2ad3f072bb19b75e6c4
Julian Gomez <julian1995ag@gmail.com>
2022-05-25 13:17:36 -0300

    Desafio 5

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -11,12 +11,15 @@ const products = [
     { id: '3', name: 'Ipad 8va', price: 1200, category: 'tablet', 
     img:'https://medias.musimundo.com/medias/00406046-143516-143516-01-143516-01.jpg-size515?context=bWFzdGVyfGltYWdlc3w1MjI1MnxpbWFnZS9qcGVnfGhjMC9oYWMvMTAyODM0OTc0NTU2NDYvMDA0MDYwNDYtMTQzNTE2LTE0MzUxNl8wMS0xNDM1MTZfMDEuanBnX3NpemU1MTV8MzA0NmMzNGFhZjUwMDY2YjY5MjU0MjlkMGJlYWMxODAxZjE5ODJhZmJlOTczZThmZjE2NTQxMjU5NjE2OTA4Yw', 
     stock: 10, description:'Descripcion de Ipad'},
-    {id:4,name:'Auricular',price:100,category: 'Accesorio Celular', img:'https://cdn.pocket-lint.com/r/s/1200x630/assets/images/155378-phones-review-hands-on-samsung-galaxy-s21-ultra-image1-luae09ici4.JPG', stock:15},
-    {id:5,name:'Mouse',price:200, category: 'Accesorio Computadora',img:'https://cdn.pocket-lint.com/r/s/1200x630/assets/images/155378-phones-review-hands-on-samsung-galaxy-s21-ultra-image1-luae09ici4.JPG', stock:15},
-    {id:6,name:'Teclado',price:300, category: 'Accesorio Computadora',img:'https://cdn.pocket-lint.com/r/s/1200x630/assets/images/155378-phones-review-hands-on-samsung-galaxy-s21-ultra-image1-luae09ici4.JPG', stock:15},
-    {id:7,name:'Camara Web',price:500,category: 'Accesorio Computadora', img:'https://cdn.pocket-lint.com/r/s/1200x630/assets/images/155378-phones-review-hands-on-samsung-galaxy-s21-ultra-image1-luae09ici4.JPG', stock:15},
-    {id:8,name:'HHD',price:900,category: 'Componente Computadora', img:'https://cdn.pocket-lint.com/r/s/1200x630/assets/images/155378-phones-review-hands-on-samsung-galaxy-s21-ultra-image1-luae09ici4.JPG', stock:15},
-    {id:9,name:'SSD',price:1000,category: 'Componente Computadora', img:'https://http2.mlstatic.com/D_NQ_NP_670396-MLA48158690358_112021-O.webp', stock:15},
+    {id:4,name:'Auricular',price:100,category: 'Accesorio Celular', 
+    img:'https://cdn.pocket-lint.com/r/s/1200x630/assets/images/155378-phones-review-hands-on-samsung-galaxy-s21-ultra-image1-luae09ici4.JPG', stock:15,description:'Descripcion de auricular'},
+    {id:5,name:'Mouse',price:200, category: 'Accesorio Computadora',img:'https://cdn.pocket-lint.com/r/s/1200x630/assets/images/155378-phones-review-hands-on-samsung-galaxy-s21-ultra-image1-luae09ici4.JPG', stock:15,description:'Descripcion de mouses'},
+    {id:6,name:'Teclado',price:300, category: 'Accesorio Computadora',img:'https://cdn.pocket-lint.com/r/s/1200x630/assets/images/155378-phones-review-hands-on-samsung-galaxy-s21-ultra-image1-luae09ici4.JPG', stock:15,description:'Descripcion de teclado'},
+    {id:7,name:'Camara Web',price:500,category: 'Accesorio Computadora', img:'https://cdn.pocket-lint.com/r/s/1200x630/assets/images/155378-phones-review-hands-on-samsung-galaxy-s21-ultra-image1-luae09ici4.JPG', stock:15,description:'Descripcion de camara'},
+    {id:8,name:'HHD',price:900,category: 'Componente Computadora', img:'https://cdn.pocket-lint.com/r/s/1200x630/assets/images/155378-phones-review-hands-on-samsung-galaxy-s21-ultra-image1-luae09ici4.JPG', stock:15,description:'Descripcion de hhd'},
+    {id:9,name:'SSD',price:1000,category: 'Componente Computadora', 
+    img:'https://http2.mlstatic.com/D_NQ_NP_670396-MLA48158690358_112021-O.webp',
+     stock:15,description:'Descripcion de ssd'},
     
 ]
 
--- src/componentes/Item/Item.js
+++ src/componentes/Item/Item.js
@@ -2,18 +2,24 @@ import React from 'react'
 
 
 const Item = ({data,addToCart}) => {
-    let{id,name,price,img} = data;
+    let{id,name,price,img,description} = data;
   return (
+<>   
 
-    <div style={{border:"thin solid gray", padding:"0.5rem", marginTop:"5px"}}>
-        <h4>{name}</h4>
+ <div style={{border:"thin solid gray", padding:"0.5rem", marginTop:"5px"} }>
+        <h4 style={{fontSize:'18px'}}>{name}</h4>
         <img alt='asd' style={{height:"30%", width:"30%"}} src={img}></img>
-        <h5>${price}.00</h5>
+        <h5 style={{fontSize:'14px'}}>${price}.00</h5>
         <div>
-
-        <button className='btn btn-info'  onClick={()=>addToCart(id)}>Agregar</button>
+            <button className='btn' style={{fontSize:'12px', borderColor:'#000000'}} onClick={()=>addToCart(id)}>Agregar</button>
         </div>
+    <div>
+        <p>{description}</p>
     </div>
+    </div>
+    
+    </>
+
   )
 }
 
